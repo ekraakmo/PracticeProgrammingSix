e32565af50b850a95e0befb0d3c0e950c316d680
Edvard Kraakmo <edv.kraakmo@gmail.com>
2022-04-04 19:57:20 -0500

    First project added to GitHub

--- src/main/java/kraakmo/learninghowtojava/waitawhile/WaitAWhile.java
+++ src/main/java/kraakmo/learninghowtojava/waitawhile/WaitAWhile.java
@@ -3,5 +3,15 @@ package kraakmo.learninghowtojava.waitawhile;
 public class WaitAWhile {
     public static void main(String[] args) {
 
+        int timeNow = 5;
+        int bedTime = 10;
+
+        while (timeNow < bedTime) {
+            System.out.println("It's only " + timeNow + " o'clock!");
+            System.out.println("I think I'll stay up just a liiiiiitle longer....");
+            timeNow++;
+        }
+        System.out.println("Oh. It's " + timeNow + " o'clock,");
+        System.out.println("Guess I should go to bed...");
     }
 }
